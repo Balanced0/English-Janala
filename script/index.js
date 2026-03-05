8579fc6258c05701672cdc568dddd6b88403e20e
balanced0 <alvichowdhury013@gmail.com>
2026-03-06 01:15:48 +0600

    Made the words section take words from API

--- script/index.js
+++ script/index.js
@@ -24,8 +24,17 @@ const showWords = (words) =>{
     words.forEach((word) => {
         const wordCard = document.createElement("div");
         wordCard.innerHTML = `
-            
+            <div class="bg-white rounded-xl text-center p-14">
+                <h5 class="text-3xl font-bold mb-[24px]">${word.word}</h5>
+                <p class="text-xl font-medium mb-[24px]">Meaning /Pronounciation</p>
+                <p class="text-3xl font-semibold font-bangla">"${word.meaning} / ${word.pronunciation}"</p>
+                <div class="flex justify-between mt-[56px]">
+                    <button class="bg-[#1A91FF10] w-14 h-14 rounded-xl hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                    <button class="bg-[#1A91FF10] w-14 h-14 rounded-xl hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+                </div>
+            </div>
         `;
+        wordContainer.append(wordCard);
     });
 }
 loadLessons();
