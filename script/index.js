fc5c259449dc444c7e80853a894a86b066f44dbc
balanced0 <alvichowdhury013@gmail.com>
2026-03-06 06:29:42 +0600

    Handle Errors , empty lessons for Better UX

--- script/index.js
+++ script/index.js
@@ -21,13 +21,23 @@ const showLessons = (lessons) => {
 const showWords = (words) =>{
     const wordContainer = document.getElementById("words-container");
     wordContainer.innerHTML = "";
+    if(words.length === 0){
+        const emptyCard = document.createElement("div");
+        emptyCard.className = "col-span-full text-center flex flex-col justify-center";
+        emptyCard.innerHTML = `
+            <img src = "./assets/alert-error.png" class="mx-auto mt-[74px]">
+                <p class="text-sm font-bangla mb-[12px] mt-[15px] text-[#79716B]">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+                <p class="text-4xl font-bangla font-medium mv-[64px]">নেক্সট Lesson এ যান</p>
+        `;
+        wordContainer.append(emptyCard);
+    }
     words.forEach((word) => {
         const wordCard = document.createElement("div");
         wordCard.innerHTML = `
-            <div class="bg-white rounded-xl text-center p-14">
-                <h5 class="text-3xl font-bold mb-[24px]">${word.word}</h5>
+            <div class="bg-white h-full rounded-xl text-center p-14 flex flex-col justify-between">
+                <h5 class="text-3xl font-bold mb-[24px]">${word.word ? word.word : "শব্দটি নেই"}</h5>
                 <p class="text-xl font-medium mb-[24px]">Meaning /Pronounciation</p>
-                <p class="text-3xl font-semibold font-bangla">"${word.meaning} / ${word.pronunciation}"</p>
+                <p class="text-3xl font-semibold font-bangla">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি"} / ${word.pronunciation ? word.pronunciation : "উচ্চারণ পাওয়া যায়নি"}"</p>
                 <div class="flex justify-between mt-[56px]">
                     <button class="bg-[#1A91FF10] w-14 h-14 rounded-xl hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
                     <button class="bg-[#1A91FF10] w-14 h-14 rounded-xl hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
